80636ac307b5188e89d3838afcb2f6c25088c99d
Keystone__ <76769151+twg0@users.noreply.github.com>
2022-08-14 00:34:25 +0900

    Spring Data JPA

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -12,25 +12,31 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
+/*
     private EntityManager em;
 
     @Autowired
     public SpringConfig(EntityManager em) {
         this.em = em;
     }
+*/
+    private final MemberRepository memberRepository;
 
-    private DataSource dataSource;
-
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository());
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+
+/*    @Bean
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+    }*/
 }
