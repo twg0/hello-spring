fc9ea754c318d3e32cec31121b57e0fa17427c45
Keystone__ <76769151+twg0@users.noreply.github.com>
2022-08-14 21:06:24 +0900

    AOP

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -23,25 +23,28 @@ public class MemberService {
     }
 
     /*
-    * 회원 가입
-    * */
+     * 회원 가입
+     * */
     public Long join(Member member) {
+
+
         // 같은 이름의 중복 회원 x
         validateDuplicateMember(member); // c a s T
-
         memberRepository.save(member);
         return member.getId();
+
     }
 
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName()) // 지저분한 optional 을 지움
-            .ifPresent(m -> {
-                throw new IllegalStateException("이미 존재하는 회원입니다.");
-            });
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
     }
+
     /*
-    * 전체 회원 조회
-    * */
+     * 전체 회원 조회
+     * */
     public List<Member> findMembers() {
         return memberRepository.findAll();
     }
